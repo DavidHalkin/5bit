26870c1b6f232d34c23dd5e1611b267345e1e763
Halkin David <northfire@maail.ru>
2018-12-13 23:59:23 +0200

    почти фикс

--- app/js/main.js
+++ app/js/main.js
@@ -6,3 +6,26 @@ $(document).ready(function() {
 
 
 });
+
+// for ie object-fit
+var userAgent, ieReg, ie;
+userAgent = window.navigator.userAgent;
+ieReg = /msie|Trident.*rv[ :]*11\./gi;
+ie = ieReg.test(userAgent);
+
+// if(ie) {
+//   $(".article_fix_ie_js").each(function () {
+//     var $container = $(this),
+//         imgUrl = $container.find("img").wrap("<div class='ie-holder'></div>").prop("src");
+//     if (imgUrl) {
+//       $container.find(".ie-holder").css("background", 'url(' + imgUrl + ')').addClass("custom-object-fit");
+//     }
+//   });
+// }
+
+
+if(ie) {
+  $(".article_fix_ie_js").each(function () {
+    var $container = $(this),hld = $container.wrap("<div class='ie_article_holder'></div>");
+  });
+}
