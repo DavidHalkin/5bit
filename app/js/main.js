ab02f190b73a1e9656cb539a6d7fd325829402dc
Halkin David <northfire@maail.ru>
2018-12-17 19:50:11 +0200

    final

--- app/js/main.js
+++ app/js/main.js
@@ -3,26 +3,16 @@ $(function() {
   jcf.replaceAll();
 });
 $(document).ready(function() {
-
-
+	$('.custom-file-input').on('change',function(){
+	  $(this).next('.custom-file-label').addClass("selected").html($(this).val());
+	})
 });
 
-// for ie object-fit
+// for ie home full-article height
 var userAgent, ieReg, ie;
 userAgent = window.navigator.userAgent;
 ieReg = /msie|Trident.*rv[ :]*11\./gi;
 ie = ieReg.test(userAgent);
-
-// if(ie) {
-//   $(".article_fix_ie_js").each(function () {
-//     var $container = $(this),
-//         imgUrl = $container.find("img").wrap("<div class='ie-holder'></div>").prop("src");
-//     if (imgUrl) {
-//       $container.find(".ie-holder").css("background", 'url(' + imgUrl + ')').addClass("custom-object-fit");
-//     }
-//   });
-// }
-
 
 if(ie) {
   $(".article_fix_ie_js").each(function () {
--- gulpfile.js
+++ gulpfile.js
@@ -118,6 +118,7 @@ gulp.task('scripts', function() {
 		'bower_components/jcf/dist/js/jcf.select.js',
 		'bower_components/jcf/dist/js/jcf.js',
 		'bower_components/bootstrap/dist/js/bootstrap.bundle.min.js',
+		'bower_components/smartwizard/dist/js/jquery.smartWizard.min.js',
 		'bower_components/fancybox/dist/jquery.fancybox.min.js'
 		])
 		// .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
@@ -130,6 +131,7 @@ gulp.task('scripts', function() {
 gulp.task('css-libs', ['sass'], function() {
 	return gulp.src([ // Берем все необходимые библиотеки
 		'bower_components/jcf/dist/css/theme-minimal/jcf.css',
+		'bower_components/smartwizard/dist/css/smart_wizard.min.css',
 		'bower_components/fancybox/dist/jquery.fancybox.min.css'
 		])
 		.pipe(sourcemaps.init())
